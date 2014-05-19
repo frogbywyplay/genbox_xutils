299206e5272fd24099888e0c4b6e0f243b3bd5a5
ptisserand <ptisserand@wyplay.com>
2014-05-19 13:40:18 +0200

    setup.py: updated version to 2.3.0

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ def find_packages(dir):
 
 setup(
     name = "xutils",
-    version = "2.2.11",
+    version = "2.3.0",
     description = "Xutils tools for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",
