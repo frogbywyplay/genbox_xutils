154bdc42996bb90a055b2a8e836e5c6ad66dcb9f
ghaderer <ghaderer@wyplay.com>
2015-06-04 14:44:15 +0200

    Release version 2.4.0.

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ def find_packages(dir):
 
 setup(
     name = "xutils",
-    version = "2.3.2",
+    version = "2.4.0",
     description = "Xutils tools for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",
